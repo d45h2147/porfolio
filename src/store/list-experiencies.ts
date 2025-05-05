b34e2514a7d625198ca589fc0dbdbce77e13a842
Daren Salazar <sdaren6@gmail.com>
2025-05-05 08:38:20 -0500

    feat: Enhance banner component with improved interactivity and visuals
    feat: Add a new menu component for navigation with theme toggle functionality
    
    fix: Update global layout to include the new menu component
    
    feat: Update index page to display achievements in experience section
    
    feat: Extend experience data structure to include achievements
    
    chore: Update Tailwind CSS configuration for dark mode support

--- src/store/list-experiencies.ts
+++ src/store/list-experiencies.ts
@@ -14,6 +14,7 @@ export type ListExperience = {
   company: string;
   description: string;
   role: string;
+  logros: string[];
   skills: Skill[];
   duration: Duration;
   companyUrl: string;
@@ -21,18 +22,23 @@ export type ListExperience = {
 
 export const listExperience = atom<ListExperience[]>([
   {
-    company: "Sainapsis",
+    company: "Sainapsis Inc",
     role: "Desarrollador FullStack",
-    description:
-      "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 0",
+    description: `En Sainapsis, me encargue de desarrollar soluciones completas, desde el frontend hasta el backend, asegurando aplicaciones robustas y escalables.`,
+    logros: [
+      "En el frontend, integro servicios en aplicaciones nativas y web, desarrollando la lógica necesaria para sincronizar datos con bases de datos locales y creando componentes personalizados y vistas funcionales basadas en los diseños UX/UI propuestos en Figma.",
+      "En el backend, diseño y desarrollo microservicios escalables y flexibles utilizando AWS Lambda y AWS ECS, trabajando con arquitecturas como hexagonal, monorepo y monolíticas. Además, implemento funcionalidades de inteligencia artificial mediante RAG (Retrieval-Augmented Generation), utilizando consultas a LLMs para atender necesidades específicas del negocio.",
+      "Mis herramientas principales incluyen Python, FastAPI, Boto3, y AWS Powertools, con un enfoque constante en la calidad, eficiencia y escalabilidad del desarrollo.",
+    ],
     skills: [
       { name: "vue", icon: "logos:vue" },
       { name: "tailwind", icon: "logos:tailwindcss-icon" },
       { name: "typescript", icon: "logos:typescript-icon" },
+      { name: "python", icon: "logos:python" },
     ],
     duration: {
       start: "2024-04",
-      end: "2025-01",
+      end: "2025-02",
     },
     companyUrl: "https://www.sainapsis.com",
   },
@@ -41,6 +47,11 @@ export const listExperience = atom<ListExperience[]>([
     description:
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 1",
     role: "Desarrollador FullStack",
+    logros: [
+      "En el frontend, integro servicios en aplicaciones nativas y web, desarrollando la lógica necesaria para sincronizar datos con bases de datos locales y creando componentes personalizados y vistas funcionales basadas en los diseños UX/UI propuestos en Figma.",
+      "En el backend, diseño y desarrollo microservicios escalables y flexibles utilizando AWS Lambda y AWS ECS, trabajando con arquitecturas como hexagonal, monorepo y monolíticas. Además, implemento funcionalidades de inteligencia artificial mediante RAG (Retrieval-Augmented Generation), utilizando consultas a LLMs para atender necesidades específicas del negocio.",
+      "Mis herramientas principales incluyen Python, FastAPI, Boto3, y AWS Powertools, con un enfoque constante en la calidad, eficiencia y escalabilidad del desarrollo.",
+    ],
     skills: [
       { name: "vue", icon: "logos:vue" },
       { name: "tailwind", icon: "logos:tailwindcss-icon" },
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class", // Enable dark mode with class strategy
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
