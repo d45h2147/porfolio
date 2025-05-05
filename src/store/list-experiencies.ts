5cab475f17a6255abf307851fb38b6a65a4e6ba9
Daren Salazar <sdaren6@gmail.com>
2025-05-05 10:26:40 -0500

    feat: Add tailwindcss-animated and enhance experience section with new entries

--- src/store/list-experiencies.ts
+++ src/store/list-experiencies.ts
@@ -22,6 +22,27 @@ export type ListExperience = {
 
 export const listExperience = atom<ListExperience[]>([
   {
+    company: "Vanilla Byte",
+    role: "CTO",
+    description: `En Sainapsis, me encargue de desarrollar soluciones completas, desde el frontend hasta el backend, asegurando aplicaciones robustas y escalables.`,
+    logros: [
+      "En el frontend, integro servicios en aplicaciones nativas y web, desarrollando la lógica necesaria para sincronizar datos con bases de datos locales y creando componentes personalizados y vistas funcionales basadas en los diseños UX/UI propuestos en Figma.",
+      "En el backend, diseño y desarrollo microservicios escalables y flexibles utilizando AWS Lambda y AWS ECS, trabajando con arquitecturas como hexagonal, monorepo y monolíticas. Además, implemento funcionalidades de inteligencia artificial mediante RAG (Retrieval-Augmented Generation), utilizando consultas a LLMs para atender necesidades específicas del negocio.",
+      "Mis herramientas principales incluyen Python, FastAPI, Boto3, y AWS Powertools, con un enfoque constante en la calidad, eficiencia y escalabilidad del desarrollo.",
+    ],
+    skills: [
+      { name: "vue", icon: "logos:vue" },
+      { name: "tailwind", icon: "logos:tailwindcss-icon" },
+      { name: "typescript", icon: "logos:typescript-icon" },
+      { name: "python", icon: "logos:python" },
+    ],
+    duration: {
+      start: "2024-04",
+      end: "2025-02",
+    },
+    companyUrl: "https://www.sainapsis.com",
+  },
+  {
     company: "Sainapsis Inc",
     role: "Desarrollador FullStack",
     description: `En Sainapsis, me encargue de desarrollar soluciones completas, desde el frontend hasta el backend, asegurando aplicaciones robustas y escalables.`,
@@ -43,10 +64,52 @@ export const listExperience = atom<ListExperience[]>([
     companyUrl: "https://www.sainapsis.com",
   },
   {
-    company: "Proyecto 1",
+    company: "Alfa Eye",
+    role: "Desarrollador FullStack",
+    description: `En Sainapsis, me encargue de desarrollar soluciones completas, desde el frontend hasta el backend, asegurando aplicaciones robustas y escalables.`,
+    logros: [
+      "En el frontend, integro servicios en aplicaciones nativas y web, desarrollando la lógica necesaria para sincronizar datos con bases de datos locales y creando componentes personalizados y vistas funcionales basadas en los diseños UX/UI propuestos en Figma.",
+      "En el backend, diseño y desarrollo microservicios escalables y flexibles utilizando AWS Lambda y AWS ECS, trabajando con arquitecturas como hexagonal, monorepo y monolíticas. Además, implemento funcionalidades de inteligencia artificial mediante RAG (Retrieval-Augmented Generation), utilizando consultas a LLMs para atender necesidades específicas del negocio.",
+      "Mis herramientas principales incluyen Python, FastAPI, Boto3, y AWS Powertools, con un enfoque constante en la calidad, eficiencia y escalabilidad del desarrollo.",
+    ],
+    skills: [
+      { name: "vue", icon: "logos:vue" },
+      { name: "tailwind", icon: "logos:tailwindcss-icon" },
+      { name: "typescript", icon: "logos:typescript-icon" },
+      { name: "python", icon: "logos:python" },
+    ],
+    duration: {
+      start: "2024-04",
+      end: "2025-02",
+    },
+    companyUrl: "https://www.sainapsis.com",
+  },
+  {
+    company: "Energia Solar ESWindows",
+    role: "Ingeniero de desarrollo de software",
+    description: `En Sainapsis, me encargue de desarrollar soluciones completas, desde el frontend hasta el backend, asegurando aplicaciones robustas y escalables.`,
+    logros: [
+      "En el frontend, integro servicios en aplicaciones nativas y web, desarrollando la lógica necesaria para sincronizar datos con bases de datos locales y creando componentes personalizados y vistas funcionales basadas en los diseños UX/UI propuestos en Figma.",
+      "En el backend, diseño y desarrollo microservicios escalables y flexibles utilizando AWS Lambda y AWS ECS, trabajando con arquitecturas como hexagonal, monorepo y monolíticas. Además, implemento funcionalidades de inteligencia artificial mediante RAG (Retrieval-Augmented Generation), utilizando consultas a LLMs para atender necesidades específicas del negocio.",
+      "Mis herramientas principales incluyen Python, FastAPI, Boto3, y AWS Powertools, con un enfoque constante en la calidad, eficiencia y escalabilidad del desarrollo.",
+    ],
+    skills: [
+      { name: "vue", icon: "logos:vue" },
+      { name: "tailwind", icon: "logos:tailwindcss-icon" },
+      { name: "typescript", icon: "logos:typescript-icon" },
+      { name: "python", icon: "logos:python" },
+    ],
+    duration: {
+      start: "2024-04",
+      end: "2025-02",
+    },
+    companyUrl: "https://www.sainapsis.com",
+  },
+  {
+    company: "Construseñales",
+    role: "Desarrollador junior",
     description:
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 1",
-    role: "Desarrollador FullStack",
     logros: [
       "En el frontend, integro servicios en aplicaciones nativas y web, desarrollando la lógica necesaria para sincronizar datos con bases de datos locales y creando componentes personalizados y vistas funcionales basadas en los diseños UX/UI propuestos en Figma.",
       "En el backend, diseño y desarrollo microservicios escalables y flexibles utilizando AWS Lambda y AWS ECS, trabajando con arquitecturas como hexagonal, monorepo y monolíticas. Además, implemento funcionalidades de inteligencia artificial mediante RAG (Retrieval-Augmented Generation), utilizando consultas a LLMs para atender necesidades específicas del negocio.",
