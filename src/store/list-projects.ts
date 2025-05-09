7b995514a299c3b622214ba8aa5702d132e1219a
Daren Salazar <sdaren6@gmail.com>
2025-05-09 08:43:14 -0500

    feat: Rename ListProject type to Project for consistency; update related references

--- src/store/list-projects.ts
+++ src/store/list-projects.ts
@@ -6,7 +6,7 @@ type Stack = {
   icon: string;
 };
 
-export type ListProject = {
+export type Project = {
   name: string;
   description: string;
   images: string[];
@@ -20,7 +20,7 @@ export type ListProject = {
   featured: boolean;
 };
 
-export const listProject = atom<ListProject[]>([
+export const listProject = atom<Project[]>([
   {
     name: "Generador de Gráficas Dinámicas con Highcharts",
     description:
@@ -46,7 +46,6 @@ export const listProject = atom<ListProject[]>([
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 1",
     images: ["https://placehold.co/100x80", "https://placehold.co/100x80"],
     tags: ["opensource", "license MIT"],
-    // tags: [],
     stack: [
       { name: "vue", color: "blue", icon: "logos:vue" },
       { name: "tailwind", color: "blue", icon: "logos:tailwindcss-icon" },
@@ -84,7 +83,6 @@ export const listProject = atom<ListProject[]>([
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 2",
     images: ["https://placehold.co/100x80", "https://placehold.co/100x80"],
     tags: ["Proyecto personal", "opensource", "license MIT"],
-    // tags: [],
     stack: [
       { name: "react", color: "blue", icon: "logos:react" },
       { name: "tailwind", color: "blue", icon: "logos:tailwindcss-icon" },
@@ -103,7 +101,6 @@ export const listProject = atom<ListProject[]>([
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 3",
     images: ["https://placehold.co/100x80", "https://placehold.co/100x80"],
     tags: ["Privado", "license Apache 2.0"],
-    // tags: [],
     stack: [
       { name: "python", color: "blue", icon: "logos:python" },
       { name: "FastApi", color: "blue", icon: "logos:fastapi-icon" },
@@ -122,7 +119,6 @@ export const listProject = atom<ListProject[]>([
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 4",
     images: ["https://placehold.co/100x80", "https://placehold.co/100x80"],
     tags: ["LLM", "opensource", "license MIT"],
-    // tags: [],
     stack: [
       { name: "OpenAI", color: "blue", icon: "logos:openai-icon" },
       { name: "FastApi", color: "blue", icon: "logos:fastapi-icon" },
@@ -141,7 +137,6 @@ export const listProject = atom<ListProject[]>([
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 5",
     images: ["https://placehold.co/100x80", "https://placehold.co/100x80"],
     tags: ["LLM", "opensource", "license MIT"],
-    // tags: [],
     stack: [
       { name: "Java", color: "blue", icon: "logos:java" },
       { name: "Spring Boot", color: "blue", icon: "logos:spring-icon" },
@@ -160,7 +155,6 @@ export const listProject = atom<ListProject[]>([
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 6",
     images: ["https://placehold.co/100x80", "https://placehold.co/100x80"],
     tags: ["tag1", "tag2"],
-    // tags: [],
     stack: [
       { name: "Go", color: "blue", icon: "logos:go" },
       { name: "Nginx", color: "blue", icon: "logos:nginx" },
@@ -179,7 +173,6 @@ export const listProject = atom<ListProject[]>([
       "cupiditate laboriosam corrupti beatae quae quaerat, dolores in. Explicabo fugit dolore ducimus, laborum asperiores temporibus beatae 7",
     images: ["https://placehold.co/100x80", "https://placehold.co/100x80"],
     tags: ["tag1", "tag2", "tag3", "tag4"],
-    // tags: [],
     stack: [
       { name: "C", color: "blue", icon: "logos:c" },
       { name: "Docker", color: "blue", icon: "logos:docker-icon" },
@@ -215,8 +208,8 @@ export const getAllSkills = () =>
       .flat(),
   );
 
-export const extractTags = (project: ListProject) => [...new Set(project.tags)];
+export const extractTags = (project: Project) => [...new Set(project.tags)];
 
-export const extractSkills = (project: ListProject) => [
+export const extractSkills = (project: Project) => [
   ...new Set(project.stack.map((skill) => skill.name)),
 ];
